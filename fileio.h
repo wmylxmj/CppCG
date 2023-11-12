399111f6321d51aa9a41c08105990fe18b6e3497
wmy <1397322329@qq.com>
2023-11-12 21:50:58 +0800

    Add files via upload

---
+++ fileio.h
@@ -0,0 +1,38 @@
+//
+// Created by wmy on 2023/11/9.
+//
+
+#ifndef CPPCG_FILEIO_H
+#define CPPCG_FILEIO_H
+
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include "matrix.h"
+
+typedef struct {
+    unsigned int vertices[3];
+    unsigned int textures[3];
+    unsigned int normals[3];
+} Triangle;
+
+class OBJFile
+{
+public:
+    Matrix vertex_coordinates;
+    Matrix texture_coordinates;
+    Matrix normal_coordinates;
+    Triangle* triangles = nullptr;
+    void read(const std::string& fp);
+    void clear() {
+        vertex_coordinates.clear();
+        texture_coordinates.clear();
+        normal_coordinates.clear();
+        delete[] triangles;
+    }
+    ~OBJFile() {
+        clear();
+    }
+};
+
+#endif //CPPCG_FILEIO_H
